8d19e299dfccc26f7e18fabe2e7f3d2ec1d1525d
Arizetbest <arizetbest@gmail.com>
2021-02-22 19:59:29 +0200

    update

--- main.js
+++ main.js
@@ -1,42 +1,22 @@
-// let body = {Human:"boy",woman:"girl",son:"child"};
-//  let school = ["university","secondary","primary"]
+// let yellowLight = "well"
+// let blueLight = "off"
+// let redLight = "danger"
+// let dayTime = 10
 
-let yellowLight = "well"
-let blueLight = "off"
-let redLight = "danger"
-let dayTime = 10
 
-const display = document.getElementById("op")
+// const display = document.getElementById("op")
 
-const age = document.getElementById("getAge")
+// const age = document.getElementById("getAge")
+const ImgOn = "/images/onlight.jpg";
+const ImgOff = "/images/offlight.jpg";
 
-const event = document.getElementById("submit").addEventListener("click", function() {
-    //console.log(age.value)
-    display.innerHTML = "Your age is " + age.value;
+
+
+const light = document.getElementById("btnOff")
+const display = document.getElementById("btnOn").addEventListener("click", function() {
+    document.getElementById("myImage").src = ImgOn
+
 })
-
-// switch (yellowLight, blueLight) {
-//     case yellowLight:
-//         alert("on")
-//         break;
-//     case blueLight:
-//         if (blueLight !== "off") {
-//             alert("its sleeping time kids")
-//         } else if (dayTime === 10) {
-//             // document.getElementById("operations").innerHTML = "The blue light is on";
-//             v.innerHTML = "Hello JavaScript";
-//             v.style.background = "purple"
-//             v.style.height = "200px"
-//         }
-//         break;
-//     case redLight:
-//         if (redLight === "danger") {
-//             alert("showing Danger");
-//         }
-
-//         break;
-//     default:
-//         let text = "`The Bulbs are ${10} and none is good`";
-//         alert("get a new bulb")
-
-// }
+document.getElementById("btnOff").addEventListener("click", function() {
+    document.getElementById("myImage").src = ImgOff
+})
